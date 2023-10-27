ae2cc055f2f4c20ba05bac8363a1c4e6ea41dd19
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-10-27 23:13:09 +0300

    feat: section hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -39,20 +39,22 @@ export default {
         text: "0px 4px 4px rgba(0, 0, 0, 0.25)",
       },
       fontSize: {
+        16: ["16px", { lineHeight: "22.4px", letterSpacing: "0.16px" }],
         18: ["18px", { lineHeight: "25.2px", letterSpacing: "0.18px" }],
-        16: ["16px", { lineHeight: "20px" }],
-        "16l": ["16px", { lineHeight: "22px" }],
+        24: ["24px", { lineHeight: "33.6px", letterSpacing: "0.24px" }],
+        48: ["48px", { lineHeight: "67.2px", letterSpacing: "0.48px" }],
+        64: ["64px", { lineHeight: "89.6px", letterSpacing: "0.64px" }],
+        128: ["128px", { lineHeight: "179.2px", letterSpacing: "1.28px" }],
       },
       backgroundImage: {
-        // background: linear-gradient(359deg, #141A19 7.33%, rgba(0, 0, 0, 0.00) 77.68%), linear-gradient(180deg, #141A19 3.18%, rgba(0, 0, 0, 0.00) 84.14%), url(<path-to-image>), lightgray -527.215px -213.685px / 368.495% 229.432% no-repeat;
-        // modal:
-        //   "linear-gradient(359deg, #141A19 7.33%, rgba(0, 0, 0, 0.00) 100%), url('./images/mob/mob-reviews.jpg'), var(--bg-modal)",
         modal: "url('./images/mob/mob-reviews.jpg'), var(--bg-modal)",
         modal2x: "url('./images/mob/mob-reviews@2x.jpg'), var(--bg-modal)",
-        "hero-mob":
-          "url('./images/mobile/hero_mob-min.jpg'), var(--bg-gradient)",
-        "hero-mob2x":
-          "url('./images/mobile/hero_mob-min@2x.jpg'), var(--bg-gradient)",
+        "hero-mob": "url('./images/mob/mob-hero.jpg'), var(--bg-hero)",
+        "hero-mob2x": "url('./images/mob/mob-hero@2x.jpg'), var(--bg-hero)",
+        "hero-tab": "url('./images/tab/tab-hero.jpg'), var(--bg-hero)",
+        "hero-tab2x": "url('./images/tab/tab-hero@2x.jpg'), var(--bg-hero)",
+        "hero-desk": "url('./images/desk/desk-hero.jpg'), var(--bg-hero)",
+        "hero-desk2x": "url('./images/desk/desk-hero@2x.jpg'), var(--bg-hero)",
       },
     },
   },
