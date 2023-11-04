dd2f363a252fdbf284a503990a2b4a05073a3ccd
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-11-04 17:08:09 +0200

    feat: add burger menu close on click burger link

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,15 +2,23 @@ const refs = {
   mobileMenu: document.querySelector("[data-menu]"),
   mobileMenuButton: document.querySelector("[data-menu-button]"),
   mobileMenuCloseButton: document.querySelector("[data-menu-close]"),
-  body: document.querySelector("body"),
 };
 
 refs.mobileMenuButton.addEventListener("click", burgerMobile);
 
 refs.mobileMenuCloseButton.addEventListener("click", burgerMobile);
 
+refs.mobileMenu.addEventListener("click", burgerLinks);
+
 function burgerMobile() {
   refs.mobileMenu.classList.toggle("-translate-y-full");
   refs.mobileMenu.classList.toggle("backdrop-hidden");
-  refs.body.classList.toggle("overflow-hidden");
+}
+
+function burgerLinks(event) {
+  if (event.target.nodeName !== "A") {
+    return;
+  }
+
+  burgerMobile();
 }
